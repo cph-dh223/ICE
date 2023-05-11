dee1e3be83735c06438fa876811b879302c67a99
DavidAH <cph-dh223@cphbusiness.dk>
2023-05-11 11:03:29 +0200

    added baseline functionalaty to game's dataStup

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,26 @@
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Game{
     private ArrayList<Letter> letters;
     private ArrayList<Player> players;
     private Board board;
     private IUI ui;
-    private void dataSetup(){throw new UnsupportedOperationException();}
+
+    private final int defaultWidth = 15;
+    private final int defaultHeight = 15;
+    private void dataSetup(){
+        List<String> dict = new LinkedList<String>();
+        try {
+            dict = IO.getDataFromTxt("../data/Dictionary.txt");
+        } catch (FileNotFoundException e) {
+            ui.displayMessige("The dictionary file was not found please look in the data folder and make shure there is a \"Dictionary.txt\" file");
+        }
+        board = new Board(defaultWidth, defaultHeight, new HashSet(dict));
+    }
     private void startGame(){throw new UnsupportedOperationException();}
     private void mainMenu(){throw new UnsupportedOperationException();}
     private void loadSavedGame(){throw new UnsupportedOperationException();}
