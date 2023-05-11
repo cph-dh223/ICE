2b51b10fddddcb62c6e8ee323a166d001df819ff
DoctorHeyde <alexanderrasmuss@gmail.com>
2023-05-11 12:15:04 +0200

    Added HashSet to read from dictionary txt and a file scanner

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Board {
@@ -8,11 +13,13 @@ public class Board {
     private int width;
     private int height;
 
-    public Board(int width, int height, Set<String> dict){
+    public Board(int width, int height, Set<String> dict) throws FileNotFoundException {
         Tile[][] tiles = new Tile[15][15];
         width = 20;
         height = 20;
-        
+        File file = new File("Dictionary.txt");
+        Scanner sc = new Scanner(file);
+        dict = new HashSet<String>();
     }
     public void placeLetter(int posX, int posY, Letter letter){
         throw new UnsupportedOperationException();
