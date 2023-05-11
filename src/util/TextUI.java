0c7ffcf0eefd3544716abffdb7d861c59e4229cd
DavidAH <cph-dh223@cphbusiness.dk>
2023-05-11 12:27:14 +0200

    made textui and its getinput method

---
+++ src/util/TextUI.java
@@ -0,0 +1,23 @@
+package util;
+
+import java.util.Scanner;
+
+import board.Board;
+
+public class TextUI implements IUI{
+
+    Scanner scanner;
+
+    public TextUI(){
+        scanner = new Scanner(System.in);
+    }
+
+
+    @Override
+    public String getInput(String msg) {
+        System.out.println(msg);
+        return scanner.nextLine();
+    }
+
+    
+}
