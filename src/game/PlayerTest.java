835212c4cbf022015e3bd3b49c86635952345138
DoctorHeyde <alexanderrasmuss@gmail.com>
2023-05-12 11:45:30 +0200

    test class

---
+++ src/game/PlayerTest.java
@@ -0,0 +1,32 @@
+package game;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+
+    @Test
+    void setUp() {
+    }
+
+    @Test
+    void addScore() {
+    }
+
+    @Test
+    void getScore() {
+    }
+
+    @Test
+    void removeLetters() {
+    }
+
+    @Test
+    void addLetters() {
+    }
+
+    @Test
+    void getName() {
+        Assertions.();
+    }
+}
