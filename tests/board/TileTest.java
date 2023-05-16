7951935ab121a78b6fb3da74167bf4a4ea7efe6d
DoctorHeyde <93933862+DoctorHeyde@users.noreply.github.com>
2023-05-16 13:24:34 +0200

    79 board init tiles (#89)
    * Spelling fixes
    
    * comments
    
    * A lot of writing...
    
    ---------
    
    Co-authored-by: cph-dh223 <124783509+cph-dh223@users.noreply.github.com>

--- tests/board/TileTest.java
+++ tests/board/TileTest.java
@@ -1,7 +1,5 @@
 package board;
 
-//import static org.junit.Assert.assertArrayEquals;
-//import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
