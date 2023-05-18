309635eb68ec866f764507ff5ce45435c33a83b1
Laumh-exe <lauritzhauge@gmail.com>
2023-05-18 22:04:19 +0200

    TextUI and Gui shows new letters on board
    Show toBePlaced letters on board after each one is tried

--- src/game/Game.java
+++ src/game/Game.java
@@ -23,6 +23,7 @@ public class Game{
 
     public Game() {
         ui = GUI.getInstance();
+        //ui = new TextUI();
         letters = new ArrayList<Letter>();
         players = new ArrayList<Player>();
         dataSetup();
